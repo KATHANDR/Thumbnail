42f2d2473419f8d8323e5e2885649d190d0dc94c
KATHANDR <86054119+KATHANDR@users.noreply.github.com>
2021-08-31 11:23:56 +0530

    Add files via upload

---
+++ Video.js
@@ -0,0 +1,21 @@
+  
+import React from 'react';
+import 'video-react/dist/video-react.css';
+import { Player, Shortcut } from 'video-react';
+
+
+function Video() {
+    return (
+    <div>
+      
+      <video onClick="" width="320" height="240" controls 
+    poster="https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Sunset_Reeds%2C_North_Shore_Port_Lincoln_-_South_Australia.jpg/1280px-Sunset_Reeds%2C_North_Shore_Port_Lincoln_-_South_Australia.jpg">
+      <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+     
+    </video>
+     
+    </div>
+  );
+}
+
+export default Video;
